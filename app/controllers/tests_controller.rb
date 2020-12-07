5a44c199d2e8329eed607d308767056e6050de45
root <root@DESKTOP-JPP4F39.localdomain>
2020-12-08 00:09:04 +0500

    for part eight

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,50 @@
 class TestsController < ApplicationController
-  before_action :tests, only:  %i[index]
 
-  def index; end
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @test = Test.find(params[:id])
+    @test.destroy
+    redirect_to request.referrer
+  end
 
   private
 
-  def tests
-    @tests = Test.all
+  def test_params
+    params.require(:test).permit(:title, :level, :author_id, :category_id)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def question_header(question)
+    if question.new_record?
+      "<h2>Create new question for  #{@test.title} test</h2>".html_safe
+    else
+      "<h2>Edit question for #{question.test.title} test</h2>".html_safe
+    end
+  end
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    link_to "Автор: #{author}", "http://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
