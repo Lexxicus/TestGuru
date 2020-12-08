76e223c5abe99dd9608fdebb19a11970aecdfc2e
root <root@DESKTOP-JPP4F39.localdomain>
2020-12-08 07:55:55 +0500

    revisions

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  before_action :find_test, %i[show edit destroy update]
+  before_action :find_test, only: %i[show edit destroy update]
   def index
     @tests = Test.all
   end
