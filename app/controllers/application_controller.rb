65fe116fd7ccd64c9888ab81fcdc79b91f400710
root <root@DESKTOP-JPP4F39.localdomain>
2020-12-17 19:59:15 +0500

    added lacalization for russian and english languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,18 +9,26 @@ class ApplicationController < ActionController::Base
     user.admin? ? admin_tests_path : root_path
   end
 
+  def default_url_options
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
+  end
+
   protected
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(
-          :name,
-          :email,
-          :password,
-          :password_confirmation,
-          :first_name,
-          :last_name
-          )
+        :name,
+        :email,
+        :password,
+        :password_confirmation,
+        :first_name,
+        :last_name
+      )
     end
   end
 end
--- db/migrate/20201212084613_add_default_value_users_name.rb
+++ db/migrate/20201212084613_add_default_value_users_name.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueUsersName < ActiveRecord::Migration[6.0]
   def change
-    change_column_default :users, :name, "User"
+    change_column_default :users, :name, 'User'
   end
 end
